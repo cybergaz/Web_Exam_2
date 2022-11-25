5fe96e34fc33aacf389264ffe3f303142727de8d
cyber_gaz <43444535+cybergaz@users.noreply.github.com>
2022-11-25 23:10:36 +0530

    Update hello.js

--- hello.js
+++ hello.js
@@ -1,4 +1,4 @@
-var search = document.getElementById('sear');
+var search = document.getElementById('in_box');
 var form = document.getElementById('form');
 form.addEventListener("submit", (e) => {
   e.preventDefault();
